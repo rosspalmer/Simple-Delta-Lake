5b931c6cdf4c4620b78286cd0060d44d2498c951
Ross Palmer <rossbpalmer@gmail.com>
2024-02-02 10:25:27 -0700

    basic install script for spark

---
+++ setup/install-spark.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+SPARK_VERSION="3.5.0"
+SPARK_HOME="/usr/local/spark"
+
+# Bash profile file (.bashrc, etc) where environment variables are set
+BASH_PROFILE=$1
+
+wget "https://dlcdn.apache.org/spark/spark-$SPARK_VERSION/spark-$SPARK_VERSION-bin-hadoop3.tgz"
+tar xvf "spark-$SPARK_VERSION-bin-hadoop3.tgz"
+mv "spark-$SPARK_VERSION-bin-hadoop3" $SPARK_HOME
+
+echo "export SPARK_HOME=$SPARK_HOME"
+echo "export PATH=\$PATH:\$SPARK_HOME/bin" >> "$BASH_PROFILE"
