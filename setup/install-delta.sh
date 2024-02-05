718bc974bbc061759a977d6d59ae9b3e67517c26
Ross Palmer <rossbpalmer@gmail.com>
2024-02-05 16:06:20 -0700

    add install delta script

---
+++ setup/install-delta.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+DELTA_VERSION="3.1.0"
+
+/usr/bin/cat <<EOF >> "$SPARK_HOME"
+spark.jars.packages io.delta:delta-spark_2.13:$DELTA_VERSION
+spark.sql.extensions io.delta.sql.DeltaSparkSessionExtension
+spark.sql.catalog.spark_catalog org.apache.spark.sql.delta.catalog.DeltaCatalog
+EOF
