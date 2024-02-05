c492f841768e915e15399b24b50a155300c6d90e
Ross Palmer <rossbpalmer@gmail.com>
2024-02-05 15:07:09 -0700

    basic setup here script

--- setup-here.sh
+++ setup-here.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 set -e
 
-apt install git -y
-
-pushd /usr/local/src
-git clone https://github.com/rosspalmer/Simple-Delta-Lake.git
-
-pushd Simple-Delta-Lake/setup
-
 bash install-java.sh
 bash install-spark.sh
-bash install-delta.sh
