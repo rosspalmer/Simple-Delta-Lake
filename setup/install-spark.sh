707f0a5818b3f4f0274399c13e14bf1d5120010d
Ross Palmer <rossbpalmer@gmail.com>
2024-02-05 16:06:05 -0700

    set spark config in main spark script

--- setup/install-spark.sh
+++ setup/install-spark.sh
@@ -3,6 +3,7 @@ set -e
 
 SPARK_VERSION="3.5.0"
 SPARK_HOME="/usr/local/spark"
+SPARK_CONFIG="$SPARK_HOME/conf/spark-defaults.conf"
 
 # Bash profile file (.bashrc, etc) where environment variables are set
 BASH_PROFILE=$1
@@ -13,7 +14,10 @@ mv "spark-$SPARK_VERSION-bin-hadoop3" $SPARK_HOME
 
 /usr/bin/cat <<EOF >> "$BASH_PROFILE"
 export SPARK_HOME=$SPARK_HOME
+export SPARK_CONFIG=$SPARK_CONFIG
 export PATH=\$PATH:\$SPARK_HOME/bin
 EOF
 
 source "$BASH_PROFILE"
+
+cp "$SPARK_CONFIG.template" "$SPARK_CONFIG"
