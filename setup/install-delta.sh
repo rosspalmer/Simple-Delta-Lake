2ecb88f66f7932f7e75cc541c7357f8d04ac4c95
Ross Palmer <rossbpalmer@gmail.com>
2024-02-05 16:16:20 -0700

    write to config

--- setup/install-delta.sh
+++ setup/install-delta.sh
@@ -6,7 +6,7 @@ source "$BASH_PROFILE"
 
 DELTA_VERSION="3.1.0"
 
-/usr/bin/cat <<EOF >> "$SPARK_HOME"
+/usr/bin/cat <<EOF >> "$SPARK_CONFIG"
 spark.jars.packages io.delta:delta-spark_2.13:$DELTA_VERSION
 spark.sql.extensions io.delta.sql.DeltaSparkSessionExtension
 spark.sql.catalog.spark_catalog org.apache.spark.sql.delta.catalog.DeltaCatalog
