a7a45cd3c8e3e27909012e7ce4c91e2a32b739c5
Ross Palmer <rossbpalmer@gmail.com>
2024-02-05 18:11:03 -0700

    use openjdk 11 binary downloaded from archive

--- setup/install-java.sh
+++ setup/install-java.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 set -e
 
-JAVA_VERSION="17"
+# Bash profile file (.bashrc, etc) where environment variables are set
+BASH_PROFILE=$1
 
-apt install "openjdk-$JAVA_VERSION-jdk"
+JAVA_HOME=/usr/local/java
+OPENJDK_BINARY="https://github.com/adoptium/temurin11-binaries/releases/download/jdk-11.0.21%2B9/OpenJDK11U-jdk_x64_mac_hotspot_11.0.21_9.tar.gz"
+
+wget "$OPENJDK_BINARY"
+
+mkdir "$JAVA_HOME"
+tar -xvf "$OPENJDK_BINARY" -C "$JAVA_HOME"
+rm OpenJDK11*.tar.gz
+
+/usr/bin/cat <<EOF >> "$BASH_PROFILE"
+export JAVA_HOME=$JAVA_HOME
+export PATH=\$PATH:\$JAVA_HOME/bin
+EOF
+
