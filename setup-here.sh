d91dc81faa1cad4c5469757b3706770f9104a559
Ross Palmer <rossbpalmer@gmail.com>
2024-02-06 09:55:39 -0700

    include scala install on setup-here script

--- setup-here.sh
+++ setup-here.sh
@@ -6,5 +6,6 @@ BASH_PROFILE=$1
 pushd setup
 
 bash install-java.sh "$BASH_PROFILE"
+bash install-scala.sh "$BASH_PROFILE"
 bash install-spark.sh "$BASH_PROFILE"
 bash install-delta.sh "$BASH_PROFILE"
