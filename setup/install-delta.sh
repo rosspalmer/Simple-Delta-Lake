139e726a31ee1c918657083f7da48691bbec4c4b
Ross Palmer <rossbpalmer@gmail.com>
2024-02-06 15:46:46 -0700

    use scala version 2.12 for delta

--- setup/install-delta.sh
+++ setup/install-delta.sh
@@ -8,7 +8,7 @@ source "$BASH_PROFILE"
 DELTA_VERSION="3.1.0"
 
 /usr/bin/cat <<EOF >> "$SPARK_CONFIG"
-spark.jars.packages io.delta:delta-spark_2.13:$DELTA_VERSION
+spark.jars.packages io.delta:delta-spark_2.12:$DELTA_VERSION
 spark.sql.extensions io.delta.sql.DeltaSparkSessionExtension
 spark.sql.catalog.spark_catalog org.apache.spark.sql.delta.catalog.DeltaCatalog
 EOF
