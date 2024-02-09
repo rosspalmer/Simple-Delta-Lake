ef6faed28aee1e360e34b957f274afa5610b45de
Ross Palmer <rossbpalmer@gmail.com>
2024-02-09 07:20:03 -0700

    use package name

--- setup/install-java.sh
+++ setup/install-java.sh
@@ -7,14 +7,15 @@ JAVA_VERSION="11.0.21+9"
 JAVA_HOME="/usr/local/java"
 
 # Construct download URL to specific OpenJDK (Java) binary
-OPENJDK_BINARY="https://github.com/adoptium/temurin11-binaries/releases/download/jdk-${JAVA_VERSION/+/%2B}/OpenJDK11U-jdk_x64_linux_hotspot_${JAVA_VERSION/+/_}.tar.gz"
+PACKAGE_NAME="OpenJDK11U-jdk_x64_linux_hotspot_${JAVA_VERSION/+/_}.tar.gz"
+OPENJDK_BINARY="https://github.com/adoptium/temurin11-binaries/releases/download/jdk-${JAVA_VERSION/+/%2B}/$PACKAGE_NAME"
 
 # Use `wget` to download OpenJDK (Java) binary archive in current directory
 wget "$OPENJDK_BINARY"
 
-tar -xvf "OpenJDK*${JAVA_VERSION/+/_}.tar.gz"
+tar -xvf "$PACKAGE_NAME"
 mv "jdk-$JAVA_VERSION" "$JAVA_HOME"
-rm "OpenJDK*${JAVA_VERSION/+/_}.tar.gz"
+rm "$PACKAGE_NAME"
 
 # Add required environment variables to bash profile file
 /usr/bin/cat <<EOF >> "$BASH_PROFILE"
