de81b3e13ed9b52b848ebd0fd06b3257fc42b0c5
Ross Palmer <rossbpalmer@gmail.com>
2024-08-13 12:58:47 -0600

    check in run for servers

--- simple_delta/run.py
+++ simple_delta/run.py
@@ -3,7 +3,8 @@ from simple_delta.build import SimpleBuild
 from simple_delta.environment import SimpleEnvironment, SimpleEnvironmentConfig
 
 
-config = SimpleEnvironmentConfig("test", "/Users/rosspalmer/simple",  "/Users/rosspalmer/simple-test/.bashrc")
+# config = SimpleEnvironmentConfig("test", "/Users/rosspalmer/simple",  "/Users/rosspalmer/simple-test/.bashrc")
+config = SimpleEnvironmentConfig("base", "/opt/simple-lake")
 env = SimpleEnvironment(config)
 
 SimpleBuild.run(env)
