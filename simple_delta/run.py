f87847b41c4a21b3a4c4256a944a9e55a041ead2
Ross Palmer <rossbpalmer@gmail.com>
2024-08-26 17:09:50 -0600

    split config files string

--- simple_delta/run.py
+++ simple_delta/run.py
@@ -1,5 +1,6 @@
 
 import sys
+from typing import List
 
 from simple_delta.build import SimpleBuild
 from simple_delta.config import SimpleDeltaConfig, read_config
@@ -8,10 +9,10 @@ from simple_delta.environment import SimpleEnvironment
 
 if __name__ == "__main__":
 
-    config_files = sys.argv[1]
+    config_files: List[str] = sys.argv[1].split(',')
     local_host = sys.argv[2]
 
-    config = read_config(config_files)
+    config = read_config(*config_files)
     env = SimpleEnvironment(config, local_host)
 
     SimpleBuild.run(env)
