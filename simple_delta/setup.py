b924033b547542a49f171fb644fdf46a1b274cdd
Ross Palmer <rossbpalmer@gmail.com>
2024-08-28 17:14:26 -0600

    remove double quotes from variable values

--- simple_delta/setup.py
+++ simple_delta/setup.py
@@ -99,8 +99,8 @@ class SetupEnvsScript(SetupTask):
         with open(env.spark_env_sh_path(), 'a') as env_sh_file:
 
             print("Writing required environment variables")
-            env_sh_file.write(f'export SPARK_LOCAL_IP="{env.local_host}\n"')
-            env_sh_file.write(f'export SPARK_HOST_IP="{env.config.master.host}\n"')
+            env_sh_file.write(f'export SPARK_LOCAL_IP={env.local_host}\n')
+            env_sh_file.write(f'export SPARK_HOST_IP={env.config.master.host}\n')
 
             if worker_info:
                 print(f"Found worker spec: {worker_info}")
