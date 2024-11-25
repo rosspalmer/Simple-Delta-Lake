afbd1ad3f1e7987ffd84f2dab144325aad364361
Ross Palmer <rossbpalmer@gmail.com>
2024-11-25 16:40:46 -0700

    add generate template functions for config

--- simple_delta/config.py
+++ simple_delta/config.py
@@ -35,18 +35,15 @@ class SimpleDeltaConfig:
     simple_home: str
     profile_path: str
     packages: Dict[str, str]
+    master: ResourceConfig
     derby_path: str = None
     warehouse_path: str = None
     metastore_config: HiveMetastoreConfig = None
-    master: ResourceConfig = None
     workers: List[ResourceConfig] = list
     executor_memory: str = None
     jdbc_drivers: dict[str, MavenJar] = None
 
-    def get_package_version(self, package_name: str) -> str:
-        return self.packages[package_name]
-
-    def write(self, json_path: str):
+    def __str__(self):
 
         config_dict = asdict(self)
         if self.master:
@@ -56,8 +53,61 @@ class SimpleDeltaConfig:
         if self.metastore_config:
             config_dict['metastore_config'] = asdict(config_dict['metastore_config'])
 
+        return json.dumps(config_dict)
+
+    def get_package_version(self, package_name: str) -> str:
+        return self.packages[package_name]
+
+    def generate_template_json(self) -> str:
+        template = SimpleDeltaConfig(
+            name='<SETUP-NAME>',
+            simple_home='<SIMPLE-SPARK-DIR>',
+            profile_path='<PATH-TO-PROFILE-FILE>',
+            packages={
+                'java': '<OPENJDK-JAVA-VERSION>',
+                'scala': '<SPARK-COMPATIBLE-SCALA-VERSION>',
+                'spark': '<SPARK-VERSION>',
+                'delta': '<OPTIONAL-DELTA-VERSION>',
+                'hadoop': '<OPTIONAL-HADOOP-VERSION>',
+                'hive': '<OPTIONAL-HIVE-VERSION>'
+            },
+            master=ResourceConfig(
+                host='<MASTER-IP-ADDRESS>',
+                cores=4,
+                memory='4G'
+            ),
+            derby_path='<OPTIONAL-PATH-TO-LOCAL-DERBY-CATALOG>',
+            warehouse_path='<OPTIONAL-PATH-TO-LOCAL-WAREHOUSE>',
+            metastore_config=HiveMetastoreConfig(
+                db_type='<mysql/postgres/oracle>',
+                db_host='<DATABASE-IP-ADDRESS>',
+                db_port=0,
+                db_user='<DATABASE-LOGIN-USERNAME>',
+                db_pass='<DATABASE-LOGIN-PASSWORD>'
+            ),
+            workers=[
+                ResourceConfig(
+                    host='<WORKER-IP-ADDRESS>',
+                    cores=4,
+                    memory='8G',
+                    instances=8
+                )
+            ],
+            executor_memory='8G',
+            jdbc_drivers={
+                'mysql': MavenJar('TODO', 'TODO', 'TODO'),
+            }
+
+        )
+
+        return str(template)
+
+    def write(self, json_path: str):
+
+        as_string = str(self)
+
         with open(json_path, 'w') as write_file:
-            json.dump(config_dict, write_file)
+            write_file.write(as_string)
 
 
 def read_config(*json_path: str) -> SimpleDeltaConfig:
