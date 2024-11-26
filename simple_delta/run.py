6be535364b3acb8b60e047db2c3a0799607b2d4f
Ross Palmer <rossbpalmer@gmail.com>
2024-11-26 14:39:47 -0700

    reimplment run function

--- simple_delta/run.py
+++ simple_delta/run.py
@@ -3,25 +3,20 @@ import sys
 from pprint import pprint
 from typing import List
 
-from simple_delta.build import SimpleBuild
 from simple_delta.config import SimpleDeltaConfig
 from simple_delta.environment import SimpleEnvironment
 
 
 if __name__ == "__main__":
 
-    # config_files: List[str] = sys.argv[1].split(',')
-    # local_host = sys.argv[2]
-    #
-    # config = read_config(*config_files)
-    # env = SimpleEnvironment(config, local_host)
-    #
-    # SimpleBuild.run(env)
+    config_files: List[str] = sys.argv[1].split(',')
+    if len(sys.argv) > 1:
+        local_host = sys.argv[2]
+    else:
+        local_host = ''
 
-    file = '/Users/rosspalmer/Desktop/sega.json'
+    config = SimpleDeltaConfig.read(*config_files)
+    env = SimpleEnvironment(config, local_host)
 
-    config = SimpleDeltaConfig.read(file)
-    env = SimpleEnvironment(config)
-
-    pprint(env)
+    env.sync()
 
