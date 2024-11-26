cb16e3e11c3ba1ada6f30daba8abf1e509b08d5c
Ross Palmer <rossbpalmer@gmail.com>
2024-11-26 11:52:24 -0700

    template updates

--- simple_delta/templates.py
+++ simple_delta/templates.py
@@ -1,6 +1,6 @@
 from typing import Any, Callable, Dict
 
-from simple_delta.config import SimpleDeltaConfig
+from simple_delta.config import SimpleDeltaConfig, ResourceConfig
 
 
 DEFAULT_VERSIONS = {
@@ -14,20 +14,51 @@ DEFAULT_VERSIONS = {
 
 
 DEFAULT_JDBC = {
-
+    "postgres": ["org.postgresql", "postgresql", "42.7.4", "org.postgresql.Driver"]
 }
 
 
 class Templates:
 
     @staticmethod
-    def generate(template_name: str, **kwargs):
+    def generate(template_name: str, with_delta: bool, with_hadoop: bool = False, **kwargs):
+
+        name = kwargs['name']
+        simple_home = kwargs['simple_home']
+        profile_path = kwargs['profile_path']
+
+        packages = DEFAULT_VERSIONS.copy()
+        if not with_delta:
+            del packages['delta']
+        if not with_hadoop:
+            del packages['hadoop']
+
+        driver = kwargs['driver']
+
+        env = SimpleDeltaConfig(
+            name, simple_home, profile_path, packages, driver
+        )
+
         match template_name:
-            case "standalone_delta_master":
-                return Templates._generate_standalone_delta_master(kwargs)
+
+            case "standalone":
+                return Templates._generate_standalone(kwargs)
+
+            case "hive_metastore":
+                metastore_db = kwargs['metastore_db']
+                with_delta = kwargs['with_delta']
+                return Templates._generate_hive(metastore_db, with_delta)
+
+    def _update_kwargs(self, template_name: str, kwargs: dict[str, Any]) -> dict[str, Any]:
+        match template_name:
+            case "standalone":
+                return kwargs
+            case "hive_metastore":
+                return Templates._generate_hive_metastore(metastore_db, with_delta)
+
 
     @staticmethod
-    def _generate_standalone_delta_master(kwargs: Dict[str, Any]) -> SimpleDeltaConfig:
+    def _generate_standalone(kwargs: dict[str, Any]) -> SimpleDeltaConfig:
 
         name = kwargs['name']
         simple_home = kwargs['simple_home']
@@ -37,10 +68,30 @@ class Templates:
         del packages['hadoop']
 
         env = SimpleDeltaConfig(
-            name, simple_home, profile_path,
-            packages, **kwargs
+            name, simple_home, profile_path, packages,
+            **kwargs
         )
+
+        driver = kwargs['driver']
 
         return env
 
+    @staticmethod
+    def _update_kwargs_metastore(kwargs: dict[str, Any]) -> SimpleDeltaConfig:
 
+        name = kwargs['name']
+        simple_home = kwargs['simple_home']
+        profile_path = kwargs['profile_path']
+
+        with_delta = kwargs.get('with_delta', False)
+
+        packages = DEFAULT_VERSIONS.copy()
+        del packages['hadoop']
+        if not with_delta:
+            del packages['delta']
+
+        driver = kwargs['driver']
+
+        env = SimpleDeltaConfig(
+            name, simple_home, profile_path, packages, driver
+        )
